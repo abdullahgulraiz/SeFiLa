7fa27a2341c3e0c5a206350cc95feb23523dcfbd
Abdullah Gulraiz <abdullah.gulraiz@tum.de>
2022-02-21 17:36:15 +0100

    Added parsers for ZAP and Arachni

--- pages/label.js
+++ pages/label.js
@@ -23,122 +23,7 @@ export default function Label() {
 
 const GenerateDS = (props) => {
     // --- State variables ---
-    const [processedFindings, setProcessedFindings] = useState({
-        0: {
-            "VulnerabilityID":
-                "CVE-2011-3374",
-            "PkgName":
-                "apt",
-            "InstalledVersion":
-                "1.8.2.3",
-            "Layer":
-                {
-                    "Digest":
-                        "sha256:69692152171afee1fd341febc390747cfca2ff302f2881d8b394e786af605696",
-                    "DiffID":
-                        "sha256:02c055ef67f5904019f43a41ea5f099996d8e7633749b6e606c400526b2c4b33"
-                }
-            ,
-            "SeveritySource":
-                "debian",
-            "PrimaryURL":
-                "https://avd.aquasec.com/nvd/cve-2011-3374",
-            "Description":
-                "It was found that apt-key in apt, all versions, do not correctly validate gpg keys with the master keyring, leading to a potential man-in-the-middle attack.",
-            "Severity":
-                "LOW",
-            "CweIDs":
-                [
-                    "CWE-347"
-                ],
-            "CVSS":
-                {
-                    "nvd":
-                        {
-                            "V2Vector":
-                                "AV:N/AC:M/Au:N/C:N/I:P/A:N",
-                            "V3Vector":
-                                "CVSS:3.1/AV:N/AC:H/PR:N/UI:N/S:U/C:N/I:L/A:N",
-                            "V2Score":
-                                4.3,
-                            "V3Score":
-                                3.7
-                        }
-                }
-            ,
-            "References":
-                [
-                    "https://access.redhat.com/security/cve/cve-2011-3374",
-                    "https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=642480",
-                    "https://people.canonical.com/~ubuntu-security/cve/2011/CVE-2011-3374.html",
-                    "https://seclists.org/fulldisclosure/2011/Sep/221",
-                    "https://security-tracker.debian.org/tracker/CVE-2011-3374",
-                    "https://snyk.io/vuln/SNYK-LINUX-APT-116518",
-                    "https://ubuntu.com/security/CVE-2011-3374"
-                ],
-            "PublishedDate":
-                "2019-11-26T00:15:00Z",
-            "LastModifiedDate":
-                "2021-02-09T16:08:00Z"
-        },
-        1: {
-            "VulnerabilityID":
-                "CVE-2011-3374",
-            "PkgName":
-                "apt",
-            "InstalledVersion":
-                "1.8.2.3",
-            "Layer":
-                {
-                    "Digest":
-                        "sha256:69692152171afee1fd341febc390747cfca2ff302f2881d8b394e786af605696",
-                    "DiffID":
-                        "sha256:02c055ef67f5904019f43a41ea5f099996d8e7633749b6e606c400526b2c4b33"
-                }
-            ,
-            "SeveritySource":
-                "debian",
-            "PrimaryURL":
-                "https://avd.aquasec.com/nvd/cve-2011-3374",
-            "Description":
-                "It was found that apt-key in apt, all versions, do not correctly validate gpg keys with the master keyring, leading to a potential man-in-the-middle attack.",
-            "Severity":
-                "LOW",
-            "CweIDs":
-                [
-                    "CWE-347"
-                ],
-            "CVSS":
-                {
-                    "nvd":
-                        {
-                            "V2Vector":
-                                "AV:N/AC:M/Au:N/C:N/I:P/A:N",
-                            "V3Vector":
-                                "CVSS:3.1/AV:N/AC:H/PR:N/UI:N/S:U/C:N/I:L/A:N",
-                            "V2Score":
-                                4.3,
-                            "V3Score":
-                                3.7
-                        }
-                }
-            ,
-            "References":
-                [
-                    "https://access.redhat.com/security/cve/cve-2011-3374",
-                    "https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=642480",
-                    "https://people.canonical.com/~ubuntu-security/cve/2011/CVE-2011-3374.html",
-                    "https://seclists.org/fulldisclosure/2011/Sep/221",
-                    "https://security-tracker.debian.org/tracker/CVE-2011-3374",
-                    "https://snyk.io/vuln/SNYK-LINUX-APT-116518",
-                    "https://ubuntu.com/security/CVE-2011-3374"
-                ],
-            "PublishedDate":
-                "2019-11-26T00:15:00Z",
-            "LastModifiedDate":
-                "2021-02-09T16:08:00Z"
-        }
-    });
+    const [processedFindings, setProcessedFindings] = useState({});
     const [findingFilesData, setFindingFilesData] = useState([]);
     const [formFields, setFormFields] = useState({'file': undefined, 'tool': "-1"});
     const tools = {
@@ -175,10 +60,34 @@ const GenerateDS = (props) => {
                 }
             },
         },
-        "anchore": {
-            "name": "Anchore",
-            "processingFunction": (data) => {
-                console.log("This data is processed by Anchore function");
+        "zap": {
+            "name": "ZAP",
+            "processingFunction": (data, startIndex) => {
+                const parsedData = JSON.parse(data);
+                let findingsTemp = {};
+                for (const site of Object.values(parsedData["site"])) {
+                    for (const alert of Object.values(site["alerts"])) {
+                        findingsTemp[startIndex++] = alert;
+                    }
+                }
+                return {
+                    "findingsTemp": findingsTemp,
+                    "endIndex": startIndex
+                }
+            },
+        },
+        "arachni": {
+            "name": "Arachni",
+            "processingFunction": (data, startIndex) => {
+                const parsedData = JSON.parse(data);
+                let findingsTemp = {};
+                for (const issue of Object.values(parsedData["issues"])) {
+                    findingsTemp[startIndex++] = issue;
+                }
+                return {
+                    "findingsTemp": findingsTemp,
+                    "endIndex": startIndex
+                }
             },
         },
     };
